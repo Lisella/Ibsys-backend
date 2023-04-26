f8ecdd98c1d068786bbbbd248ec6de0974d2e72a
Lisella <38525372+Lisella@users.noreply.github.com>
2023-04-26 20:49:00 +0200

    Spring Boot Initialized

---
+++ src/test/java/de/Ibsys/ibsys/IbsysApplicationTests.java
@@ -0,0 +1,13 @@
+package de.Ibsys.ibsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
