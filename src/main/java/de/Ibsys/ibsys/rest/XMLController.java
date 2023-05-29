01fa1693ef04b7469c9ddda059bb922aa1fd1367
Lisella <38525372+Lisella@users.noreply.github.com>
2023-05-29 11:05:59 +0200

    Controller Up and Running
    Running Controller

--- src/main/java/de/Ibsys/ibsys/rest/XMLController.java
+++ src/main/java/de/Ibsys/ibsys/rest/XMLController.java
@@ -2,21 +2,26 @@ package de.Ibsys.ibsys.rest;
 
 import de.Ibsys.ibsys.entity.Input;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class XMLController {
 
-    @PostMapping("/input")
+    @PostMapping("/in")
     public ResponseEntity<String> processInput(@RequestBody Input input) {
-        // Verarbeiten Sie die Input-Daten und führen Sie die entsprechenden Operationen durch
-        // ...
+
+
 
         return ResponseEntity.ok("Erfolgreich verarbeitet");
+    }
+
+    @GetMapping("/input")
+    public ResponseEntity<String> getResponse() {
+
+
+
+        return ResponseEntity.ok("ok");
     }
 }
 
