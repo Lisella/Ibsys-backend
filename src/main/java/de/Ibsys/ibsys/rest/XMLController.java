011b93765c7797eaef698349094127d4bf64c65f
Lisella <38525372+Lisella@users.noreply.github.com>
2023-05-29 11:48:51 +0200

    controller

--- src/main/java/de/Ibsys/ibsys/rest/XMLController.java
+++ src/main/java/de/Ibsys/ibsys/rest/XMLController.java
@@ -1,20 +1,22 @@
 package de.Ibsys.ibsys.rest;
 
 import de.Ibsys.ibsys.entity.Input;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class XMLController {
-
-    @PostMapping("/in")
-    public ResponseEntity<String> processInput(@RequestBody Input input) {
-
-
-
-        return ResponseEntity.ok("Erfolgreich verarbeitet");
-    }
+        @PostMapping("/in")
+        public ResponseEntity<String> processInput(@RequestBody String inputXml) {
+            try {
+                return ResponseEntity.ok(inputXml);
+            } catch (Exception e) {
+                // Hier können Sie den Umgang mit Fehlern implementieren, falls erforderlich.
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ein Fehler ist aufgetreten.");
+            }
+        }
 
     @GetMapping("/input")
     public ResponseEntity<String> getResponse() {
